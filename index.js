9de7f3a0dd6f2b0dd17b0f1bc060388cda432540
SMDT-Empty <pols.igor@bk.ru>
2023-11-07 16:16:04 +0300

    fx

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ const puppeteer = require('puppeteer');
     await page.setViewport({ width: 1800, height: 900 });
     await page.goto('https://google.com');
     await page.type('textarea[name="q"]', 'qa engineering');
-    await page.keyboard.press('Enter');
-    await page.waitForSelector("#result-stats");
-    await page.screenshot({ path: 'google.png' });
+    await page.evaluate(() => {
 
+        const search = document.querySelector(".gNO89b")
+        search.click();
+    })
+
+    await page.waitForSelector("#rso");
+
+    const href = await page.evaluate(() => {
+        const rso = document.querySelectorAll("[id='rso']")[0]
+        const website = rso.querySelectorAll('a')[0]
+        return website.getAttribute('href')
+    })
+    const newPage = await browser.newPage();
+    await newPage.goto(href);
+
+    await page.screenshot({ path: 'google.png' });
+    await newPage.screenshot({ path: 'newPage.png' });
     await browser.close();
 })();
